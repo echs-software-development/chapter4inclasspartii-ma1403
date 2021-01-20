407dc1bf815451c78e6782ec81a4e899e55836ff
ma1403 <73349536+ma1403@users.noreply.github.com>
2021-01-20 06:08:35 +0000

    1/19/21

--- Prob2.java
+++ Prob2.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 class Prob2 {
   public static void main(String[] args) {
     
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String word = scan.nextLine();
+    System.out.println(word.substring(0,1));
   }
 }
--- Prob3.java
+++ Prob3.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 class Prob3 {
   public static void main(String[] args) {
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter a word: ");
+
+  String word = scan.nextLine();
+  word= word.LowerCase();
+
+  System.out.println("Enter another word: ");
+  String word2 =scan.nextLine();
+  word2 = word2.LowerCase();
+
+  System.out.println(word.compareTo(word2));
   }
 }
