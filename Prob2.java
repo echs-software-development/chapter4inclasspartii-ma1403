b8e5a96a12df879d495fb717a6565ff33f0370e2
ma1403 <73349536+ma1403@users.noreply.github.com>
2021-01-21 16:34:18 +0000

    problem 2

--- Prob2.java
+++ Prob2.java
@@ -6,6 +6,10 @@ class Prob2 {
     Scanner scan = new Scanner(System.in);
     System.out.print("Enter a string: ");
     String word = scan.nextLine();
+
     System.out.println(word.substring(0,1));
+
+    System.out.print(word.substring(word.length()-1));
   }
+  
 }
